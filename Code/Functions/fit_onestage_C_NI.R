da447dc4891ee7ec74fbb49e8a7c2cb6791bd6b2
Mo Yin <44020608+moyinNUHS@users.noreply.github.com>
2023-07-14 10:57:41 +0800

    minor formatting of codes

--- Code/Functions/fit_onestage_C_NI.R
+++ Code/Functions/fit_onestage_C_NI.R
@@ -11,17 +11,18 @@ fit_onestage_C_NI<-function(alldata=Alldata){
   prior_int <- student_t(df = 7, loc_NI, scale_NI)
   
   nma_data<-data.frame(y=unlist(alldata[1,]),
-                       treatment=factor(unlist(alldata[2,]), levels = sort(unique(unlist(alldata[2,])))),
-                       subgroup=factor(unlist(alldata[4,]))#,
+                       treatment=factor(unlist(alldata[2,]), 
+                                        levels = sort(unique(unlist(alldata[2,])))),
+                       subgroup=factor(unlist(alldata[4,]))
                        #site=factor(unlist(alldata[5,]))
-                       )
+  )
   
   my.glm<-myTryCatch(stan_glm(y~treatment + subgroup, data = nma_data, prior = prior,
                               prior_intercept = prior_int, family = binomial(link = "logit"), 
                               cores = 1, refresh=0) )
- ### my.glm<-myTryCatch(stan_glmer(y~treatment + subgroup + (1 | site), data = nma_data, prior = prior,
+  ### my.glm<-myTryCatch(stan_glmer(y~treatment + subgroup + (1 | site), data = nma_data, prior = prior,
   #                           prior_intercept = prior_int, family = binomial(link = "logit"),
-   #                          cores = 1, refresh=0) )
+  #                          cores = 1, refresh=0) )
   #use of default priors in stan_glm are non-informative
   
   if(is.null(my.glm$error) ) #if do not have an error, model is fitted
@@ -29,35 +30,32 @@ fit_onestage_C_NI<-function(alldata=Alldata){
     my.glmm<-my.glm[[1]]
     #Treat.best<-which.min(c(0, my.glmm$coefficients[2:no_treatment]))
     #if (Treat.best==1){
-      mof<-posterior_interval(my.glmm, prob = 0.95)
-      std.err<-my.glmm$ses[2:no_treatment]
-      out<-cbind(Estimate=my.glmm$coefficients[2:no_treatment], #get_estimates(my.glmm, centrality = "mean")[2:no_treatment, 2], #for mean instead of median
+    mof<-posterior_interval(my.glmm, prob = 0.95)
+    std.err<-my.glmm$ses[2:no_treatment]
+    out<-cbind(Estimate=my.glmm$coefficients[2:no_treatment], #get_estimates(my.glmm, centrality = "mean")[2:no_treatment, 2], #for mean instead of median
                model_var=std.err^2,
                z=my.glmm$coefficients[2:no_treatment]/std.err, #get_estimates(my.glmm, centrality = "mean")[2:no_treatment, 2]/std.err,
                LL=mof[2:no_treatment, 1],
                UL=mof[2:no_treatment, 2])
-      out[which(abs(out[,1])>12),]<-NA #parameter not converged is set to NA 
-   # } else {
-   #   my.glmm<-stan_glm(y~relevel(treatment, ref = Treat.best) + subgroup, data = nma_data, prior = prior,
-   #                     prior_intercept = prior_int, family = binomial(link = "logit"), 
-   #                     cores = 1, refresh=0)
-      ###my.glmm<-stan_glmer(y~relevel(treatment, ref = Treat.best) + subgroup + (1 | site), data = nma_data, prior = prior,
-      #prior_intercept = prior_int, family = binomial(link = "logit"),
-      #cores = 1, refresh=0)
-   #   mof<-posterior_interval(my.glmm, prob = 0.95)
-   #   std.err<-my.glmm$ses[2:no_treatment]
-   #   out<-cbind(Estimate=my.glmm$coefficients[2:no_treatment],
-   #              model_var=std.err^2,
-   #              z=my.glmm$coefficients[2:no_treatment]/std.err,
-   #              LL.1=mof[2:no_treatment, 1],
-   #              UL.1=mof[2:no_treatment, 2])
-   #   out[which(abs(out[,1])>12),]<-NA #parameter not converged is set to NA 
-    }
-  }else
-  { # if there is error, do not fit model
+    out[which(abs(out[,1])>12),]<-NA #parameter not converged is set to NA 
+    # } else {
+    #   my.glmm<-stan_glm(y~relevel(treatment, ref = Treat.best) + subgroup, data = nma_data, prior = prior,
+    #                     prior_intercept = prior_int, family = binomial(link = "logit"), 
+    #                     cores = 1, refresh=0)
+    ###my.glmm<-stan_glmer(y~relevel(treatment, ref = Treat.best) + subgroup + (1 | site), data = nma_data, prior = prior,
+    #prior_intercept = prior_int, family = binomial(link = "logit"),
+    #cores = 1, refresh=0)
+    #   mof<-posterior_interval(my.glmm, prob = 0.95)
+    #   std.err<-my.glmm$ses[2:no_treatment]
+    #   out<-cbind(Estimate=my.glmm$coefficients[2:no_treatment],
+    #              model_var=std.err^2,
+    #              z=my.glmm$coefficients[2:no_treatment]/std.err,
+    #              LL.1=mof[2:no_treatment, 1],
+    #              UL.1=mof[2:no_treatment, 2])
+    #   out[which(abs(out[,1])>12),]<-NA #parameter not converged is set to NA 
+  } else { # if there is error, do not fit model
     out<-matrix(rep(NA,(no_treatment-1)*5),nrow = no_treatment-1, ncol = 5 )
     out[1,5]<-my.glm$error[1]$message
-    
   } 
   
   
@@ -74,7 +72,9 @@ fit_onestage_C_NI<-function(alldata=Alldata){
       est.contrasts<-rep(NA, no_treatment)
       est.contrasts[t_label]<-fit.coeff[t_label]
       
-    }else{ est.contrasts<-rep(NA, no_treatment) }
+    } else { 
+      est.contrasts<-rep(NA, no_treatment) 
+    }
     
     return(find.rankings(t_labelv=t_label, treat.coeff=est.contrasts) )
     
