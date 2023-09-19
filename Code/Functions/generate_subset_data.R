7b8f5b6ec280bb6c36c9db123f58809e9973b699
HollyJackso <131970797+HollyJackso@users.noreply.github.com>
2023-09-19 11:53:05 +0200

    Update generate_subset_data.R
    format function

--- Code/Functions/generate_subset_data.R
+++ Code/Functions/generate_subset_data.R
@@ -15,35 +15,35 @@ generate_subset_data<-function(k, size_pattern., pattern., res_probability_all.,
   treatment_label<-apply(assigned_treatment, 1, 
                          function(x) x[which(x==max(x))]<-pattern_s[which(x==max(x))] )
 
-assigned_site<-t(rmultinom(sp, 1, 
+  assigned_site<-t(rmultinom(sp, 1, 
                            rep(1/10, 10)))
-site_s = 1:10
-site_label<-apply(assigned_site, 1, 
+  site_s = 1:10
+  site_label<-apply(assigned_site, 1, 
                   function(x) x[which(x==max(x))]<-site_s [which(x==max(x))] )
                          
-responses<-lapply(1:length(pattern_s), 
+  responses<-lapply(1:length(pattern_s), 
                   function(j)rbinom(sum(treatment_label==pattern_s[j]), 1, res_p[j]))
                     
-assigned_treatment<-unlist(lapply(1:length(pattern_s),function(j)rep(pattern_s[j],length(responses[[j]])) ))
+  assigned_treatment<-unlist(lapply(1:length(pattern_s),function(j)rep(pattern_s[j],length(responses[[j]])) ))
   
-pattern_lab<-rep(k,  sp)#unlist(sapply(1:no_pattern, function(j)rep(j,  sp)) )
-responses<-unlist(responses)
+  pattern_lab<-rep(k,  sp)#unlist(sapply(1:no_pattern, function(j)rep(j,  sp)) )
+  responses<-unlist(responses)
 
-treat.site.comb<-cbind(responses=responses, 
+  treat.site.comb<-cbind(responses=responses, 
                        treatment_label=assigned_treatment, 
                        pattern_lab=pattern_lab,
                        site_label = site_label)
-treat.site.comb<-as.data.frame(treat.site.comb)
-treat.site.comb$treatment_label<-as.numeric(treat.site.comb$treatment_label)
-treat.site.comb$site_label<-as.numeric(treat.site.comb$site_label)
+  treat.site.comb<-as.data.frame(treat.site.comb)
+  treat.site.comb$treatment_label<-as.numeric(treat.site.comb$treatment_label)
+  treat.site.comb$site_label<-as.numeric(treat.site.comb$site_label)
   
-if (differsite > 0){
-  res_p1 = res_probability_all_site.[k, pattern_s]
+  if (differsite > 0){
+    res_p1 = res_probability_all_site.[k, pattern_s]
     for (j in 1:length(pattern_s)){
       treat.site.comb$responses[treat.site.comb$treatment_label==pattern_s[j] & treat.site.comb$site_label<= differsite ] = 
         rbinom(1, 1, res_p1[j])
+    }
   }
-}
                                     
   return(list(responses=treat.site.comb$responses, 
               treatment_label=treat.site.comb$assigned_treatment, 
