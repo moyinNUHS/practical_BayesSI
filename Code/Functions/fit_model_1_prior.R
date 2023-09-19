045cb5bd88ecf19b6515033f53431ac329187ea4
HollyJackso <131970797+HollyJackso@users.noreply.github.com>
2023-09-19 12:00:33 +0200

    Update fit_model_1_prior.R
    Tidy function
    (delete commented functions)

--- Code/Functions/fit_model_1_prior.R
+++ Code/Functions/fit_model_1_prior.R
@@ -16,8 +16,6 @@ fit_model_1_prior <- function(nma_data_prior,
     glm(y ~ -1 + treatment + subgroup, family = "binomial", data =
           nma_data_prior)
   my.glm_prior_coeff <- my.glm_prior$coefficients
-  ### my.glm_prior<-glmer(y~treatment+ subgroup + (1 | site),family="binomial",data=nma_data_prior)
-  ### my.glm_prior_coeff<-fixef(my.glm_prior)
   
   prior <-
     normal(location = my.glm_prior_coeff[1:(no_treatment + no_p - 1)], 
@@ -50,49 +48,23 @@ fit_model_1_prior <- function(nma_data_prior,
 #      update(my.glm$value, iter = 500)
 #    )
 #  }
-  ###my.glm<-myTryCatch(stan_glmer(y~treatment + subgroup + (1 | site), data = nma_data, prior = prior,
-  #                        prior_intercept = prior_int, family = binomial(link = "logit"),
-  #                       cores = 1, refresh=0) )
   
   if (is.null(my.glm$error))
     #if do not have an error, model is fitted
   {
     my.glmm <- my.glm[[1]]
-    #Treat.best<-which.min(c(0, my.glmm$coefficients[2:no_treatment]))
-    #if (Treat.best==1){
     
     if (bonferr == T) {
-      
       out = glm_output_stan_bonferr(model = my.glmm, p, no_treatment)
-      
     } else {
-      
       out = glm_output_stan_nocorrection(model = my.glmm, p, no_treatment)
     }
-    
-    #} else{
-    #   my.glmm<-stan_glm(y~relevel(treatment, ref = Treat.best) + subgroup, data = nma_data, prior = prior,
-    #                    prior_intercept = prior_int, family = binomial(link = "logit"),
-    #                    cores = 1, refresh=0)
-    #   ###my.glmm<-stan_glmer(y~relevel(treatment, ref = Treat.best) + subgroup + (1 | site), data = nma_data, prior = prior,
-    #   #prior_intercept = prior_int, family = binomial(link = "logit"),
-    #   #cores = 1, refresh=0)
-    #   mof<-posterior_interval(my.glmm, prob = 0.95)
-    #   std.err<-my.glmm$ses[2:no_treatment]
-    #   out<-cbind(Estimate=my.glmm$coefficients[2:no_treatment],
-    #              model_var=std.err^2,
-    #              z=my.glmm$coefficients[2:no_treatment]/std.err,
-    #              LL.1=mof[2:no_treatment, 1],
-    #              UL.1=mof[2:no_treatment, 2])
-    #   out[which(abs(out[,1])>12),]<-NA #parameter not converged is set to NA
-    # }
     
   } else {
     # if there is error, do not fit model
     out <-
       matrix(rep(NA, (no_treatment - 1) * 5), nrow = no_treatment - 1, ncol = 5)
     out[1, 5] <- my.glm$error[1]$message
-    
   }
   
   # for each subgroup, prepare the coefficients to identify rankings
