ead55387343d96bb4c9efca2a29a0e4eba2d68d0
HollyJackso <131970797+HollyJackso@users.noreply.github.com>
2023-09-26 17:52:59 +0200

    Update rank.v.mat.R
    Replace sim_data[[]] with Trial_Treat_lab_vec

--- Code/Functions/rank.v.mat.R
+++ Code/Functions/rank.v.mat.R
@@ -2,8 +2,8 @@
 # 1st row = best treatments 
 # 2nd row indicates 1 if any models did not fit 
 
-rank.v.mat <- function(no_p, alldata, my.glm, out){
-  rank.v <- sapply(1:no_p, function(x) {prep.coeff(x, alldata, my.glm, out)})
+rank.v.mat <- function(no_p, Trial_Treat_lab_vec, my.glm, out){
+  rank.v <- sapply(1:no_p, function(x) {prep.coeff(x, Trial_Treat_lab_vec, my.glm, out)})
   
   colnames(rank.v) <- sapply(1:no_pattern, function(i) paste("pattern", i))
   row.names(rank.v) <- c("suggested treatment", "model.not.fit")
