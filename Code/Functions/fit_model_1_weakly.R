2258eda518aca8b990c65953cd48dcab5a74c5ad
HollyJackso <131970797+HollyJackso@users.noreply.github.com>
2023-12-23 12:29:27 +0100

    Update fit_model_1_weakly.R
    Remove Bonferroni/Dunnett type I error correction & update alt_hyp

--- Code/Functions/fit_model_1_weakly.R
+++ Code/Functions/fit_model_1_weakly.R
@@ -1,13 +1,16 @@
 ## --------------------------------------------------------------------------- #
 # model 1 extension non-informative prior: fit fixed effect model to all data  #
 # ---------------------------------------------------------------------------- #
+ 
+ fit_model_1_weakly <- function(nma_data, 
+                               Trial_Treat_lab_vec, 
+                               alternative = 'two-sided', 
+                               p = 0.2) {
+  #Specify p depending on alt_hyp
+  if (alt_hypothesis == "two.sided") {p.val=p
+  } else if (alt_hypothesis == "one.sided") {p.val=p/2
+  }
 
-fit_model_1_weakly <- function(nma_data, 
-                           Trial_Treat_lab_vec, 
-                           alternative = 'two-sided', 
-                           p = 0.2,
-                           bonferr = T) {
-  
   # number of patterns
   no_p <- no_pattern
   
@@ -17,20 +20,20 @@ fit_model_1_weakly <- function(nma_data,
   prior <- student_t(df = 7, loc_NI, scale_NI)
   prior_int <- student_t(df = 7, loc_NI, scale_NI)
   
-# model 
- my.glm <- 
-  myTryCatch(
-    stan_glm(
-    y ~ -1 + treatment + subgroup,
-    data = nma_data,
-    prior = prior,
-    prior_intercept = prior_int,
-    family = binomial(link = "logit"),
-    chains = 8,  #defult is 4 chains
-    iter = 2000, #defult is 2000; run 1000, if not enough, then run until reaching 2000
-    cores = 1,
-    refresh = 0
-  )) 
+  # model 
+  my.glm <- 
+    myTryCatch(
+      stan_glm(
+        y ~ -1 + treatment + subgroup,
+        data = nma_data,
+        prior = prior,
+        prior_intercept = prior_int,
+        family = binomial(link = "logit"),
+        chains = 8,  #default is 4 chains
+        iter = 2000, #default is 2000
+        cores = 1,
+        refresh = 0
+      )) 
   
   if (is.null(my.glm$error))
     #if do not have an error, model is fitted
@@ -38,16 +41,12 @@ fit_model_1_weakly <- function(nma_data,
     # extract model output 
     my.glmm <- my.glm[[1]]
     
-    if (bonferr == T) {
-      out = glm_output_stan_bonferr(model =  my.glmm, p, no_treatment)
-    } else {
-      out = glm_output_stan_nocorrection(model =  my.glmm, p, no_treatment)
-    }
+    # Find Type 1 error no correction 
+    out = glm_output_stan_nocorrection(model =  my.glmm, p.val, no_treatment)
     
   } else { # if there is error, do not fit model 
     out <- matrix(rep(NA, (no_treatment) * 5), nrow = no_treatment, ncol = 5)
     out[1, 5] <- my.glm$error[1]$message
-    out[which(abs(out[, 1]) > 12), ] <- NA #parameter not converged is set to NA    
   }
   
   # for each subgroup, prepare the coefficients to identify rankings
