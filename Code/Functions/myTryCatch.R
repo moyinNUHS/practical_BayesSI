dcac9cde1a671da8c2adafeb71895eaf502fffe1
cloudy-shou <yiyun.shou@gmail.com>
2023-12-24 16:46:01 +0800

    Update myTryCatch.R

--- Code/Functions/myTryCatch.R
+++ Code/Functions/myTryCatch.R
@@ -4,13 +4,13 @@
 
 myTryCatch <- function(expr) {
   warn <- err <- NULL
-  value <- withCallingHandlers(
+   value <- withCallingHandlers(
     tryCatch(expr, error=function(e) {
       err <<- conditionMessage(e)
       NULL
-    }), warning=function(w) {
+    }, warning=function(w) {
       warn <<- append(warn, conditionMessage(w))
-      invokeRestart("muffleWarning")
     })
+  )
   list(value=value, warning=warn, error=err)
 }
