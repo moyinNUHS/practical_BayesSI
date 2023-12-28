45e2e140072334cd1ef2377eef0e1150d4130b85
cloudy-shou <yiyun.shou@gmail.com>
2023-12-28 15:33:25 +0800

    Update myTryCatch.R

--- Code/Functions/myTryCatch.R
+++ Code/Functions/myTryCatch.R
@@ -1,15 +1,47 @@
 # ---------------------------------------- #
 # to catch warning or error from model fit #
 # ---------------------------------------- #
-
 myTryCatch <- function(expr) {
   warn <- err <- NULL
    value <- withCallingHandlers(
-    tryCatch(expr, error=function(e) {
-      err <<- conditionMessage(e)
-    }, warning=function(w) {
-      warn <<- append(warn, conditionMessage(w))
-    })
+     tryCatch(expr, 
+             error = function(e) {
+               err <- conditionMessage(e)
+               return(list(value = NULL, 
+                           err = err,
+                           warn = NULL))
+              }, 
+             warning = function(w) {
+               warn <- conditionMessage(w)
+               return(list(value = suppressWarnings(expr),
+                           err = NULL,
+                           warn = warn))
+               }
+             )
   )
-  list(value=value, warning=warn, error=err)
+  
+  if ("glm" %in% class(value)|"glmerMod" %in% class(value)){
+    value <- list(value = value,
+                  err = NULL,
+                  warn = NULL)
+  }
+  
+  if (is.null(warn)){
+    if ("glmerMod" %in% class(value)){
+      if(!is.null(value@optinfo$cov$lme4$message)){
+        if(!isSingular(value)){
+          warn <- conditionMessage(w)
+          value$warn <- warn 
+        }
+      }
+      if (length(value@optinfo$warnings) > 0){
+        warn <- append(warn, unlist(value@optinfo$warnings))
+        value$warn <- warn 
+      }
+    }
+  }
+  
+  list(value=value$value, 
+       warning=value$warn, 
+       error=value$err)
 }
