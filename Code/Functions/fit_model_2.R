8ea50ed269bde946b87606b16cee87ae68bec064
cloudy-shou <yiyun.shou@gmail.com>
2024-01-08 13:08:46 +0800

    Update fit_model_2.R

--- Code/Functions/fit_model_2.R
+++ Code/Functions/fit_model_2.R
@@ -42,16 +42,21 @@ fit_model_2 <- function(nma_data,
     
     # Type 1 error no correction
       out = glm_output_nocorrection(my.glmm)
-    
+  if (!is.null(my.glm$warning)){
+      warning <- my.glm$warning
+    }
   } else {
     # if there is error, do not fit model
     out <-
       matrix(rep(NA, (no_treatment - 1) * 5), nrow = no_treatment - 1, ncol = 5)
-    out[1, 5] <- my.glm$error[1]$message
+    out[1,5] <- my.glm$error
+    warning <- my.glm$error
   }
   
   # for each subgroup, prepare the coefficients to identify rankings
   rank.v = rank.v.mat(no_p, Trial_Treat_lab_vec, my.glm, out)
   
-  return(list(contrast.est = out, ranking = rank.v))
+  return(list(contrast.est = out, 
+              ranking = rank.v,
+              warning = warning))
 }
