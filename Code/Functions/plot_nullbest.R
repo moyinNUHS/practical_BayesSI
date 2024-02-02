7c557b81ae3e2d7665d7f2ac0260e1501263e9d2
rebeccaperezMGH <131869387+rebeccaperezMGH@users.noreply.github.com>
2024-02-02 18:47:42 +0800

    Add files via upload

--- Code/Functions/plot_nullbest.R
+++ Code/Functions/plot_nullbest.R
@@ -3,13 +3,11 @@ plot_nullbest <- function(outputs, .font_size = font_size, .pt_size = pt_size, N
   
   null_data <- as.data.frame(matrix(NA, nrow = 1, ncol = 6))
   alliter_data <- as.data.frame(matrix(NA, nrow = 1, ncol = 6))
-  colnames(null_data) <- c("Treatment1", "Treatment2", "Treatment3", "Treatment4", "Method", "SampleSize")
-  colnames(alliter_data) <- c("Treatment1", "Treatment2", "Treatment3", "Treatment4", "Method", "SampleSize")
+  colnames(null_data) <- c("pattern 1", "pattern 2", "pattern 3", "pattern 4", "Method", "SampleSize")
+  colnames(alliter_data) <- c("pattern 1", "pattern 2", "pattern 3", "pattern 4", "Method", "SampleSize")
   n = parse_number(names(outputs[[ind]]))
   d <- outputs[[ind]]
-  subset_iter_fin <- as.data.frame(matrix(NA, nrow = 1, ncol = 6))
-  colnames(subset_iter_fin) <- c("Treatment1", "Treatment2", "Treatment3", "Treatment4", "Method", "SampleSize")
-  
+
   for (i in n) {
     
     sub_ind <- grep(paste("=",as.character(i), sep=" "), names(d))
@@ -22,30 +20,9 @@ plot_nullbest <- function(outputs, .font_size = font_size, .pt_size = pt_size, N
       subset_iter$Method <- rownames(subset_iter)
       
       subset_iter$SampleSize <- i
+
       
-      colnames(subset_iter) <- c("Treatment1", "Treatment2", "Treatment3", "Treatment4", "Method", "SampleSize")
-      
-      #subset_iter_fin <- rbind(subset_iter_fin, subset_iter)
-      #subset_iter_fin <- subset_iter_fin[!is.na(subset_iter_fin$Treatment1),]
-      #subset_iter_fin$Method <- rownames(subset_iter_fin)
-      iter_data <- as.data.frame(matrix(NA, nrow = nrow(subset_iter), ncol = 6))
-      colnames(iter_data) <- c("Treatment1", "Treatment2", "Treatment3", "Treatment4", "Method", "SampleSize")
-      
-      for (t in 1:4){
-        for (m in 1:nrow(subset_iter)){
-          iter_data[m,t] <- sum(subset_iter[m,1:4]==t)/4
-          iter_data[m,5] <- subset_iter[m,5]
-          iter_data[m,6] <- subset_iter[m,6]
-    }
-    
-   
-        
-  }
-  
- 
-      
-      
-      alliter_data <- rbind(alliter_data, iter_data) 
+      alliter_data <- rbind(alliter_data, subset_iter) 
       
     }
     
@@ -56,36 +33,37 @@ plot_nullbest <- function(outputs, .font_size = font_size, .pt_size = pt_size, N
   }
   
   
-  null_data <- null_data[!is.na(null_data$Treatment1),]
+  null_data <- null_data[!is.na(null_data$`pattern 1`),]
   
-  plotting <- null_data 
-  
+  plotting <- as.data.frame(matrix(NA, nrow = 1, ncol = 6))
+  colnames(plotting) <- c("Treatment 1", "Treatment 2", "Treatment 3", "Treatment 4", "Method", "Sample Size")
   plotm1 <- plotting[1,]
   
   for(q in n){
-    for (m in unique(plotting$Method)){
-    sample_meth <- subset(plotting, SampleSize == q & Method == m)
-    
-    sample_meth[,1] <- mean(sample_meth[,1])
-    sample_meth[,2] <- mean(sample_meth[,2])
-    sample_meth[,3] <- mean(sample_meth[,3])
-    sample_meth[,4] <- mean(sample_meth[,4])
-    
-    plotm1 <- rbind(plotm1, sample_meth)
+    for (m in unique(null_data$Method)){
+      sample_meth <- subset(null_data, SampleSize == q & Method == m)
+      
+     plotting[1,1] <- sum(sample_meth[, c(2,4)] == 1) / nrow(sample_meth) * 2
+     plotting[1,2] <- sum(sample_meth[, 1:4] == 2) / nrow(sample_meth) * 4
+     plotting[1,3] <- sum(sample_meth[, 1:4] == 3) / nrow(sample_meth) * 4
+     plotting[1,4] <- sum(sample_meth[, 3:4] == 4) / nrow(sample_meth) * 2
+     plotting[1,5] <- m
+     plotting[1,6] <- q
+     
+      
+      plotm1 <- rbind(plotm1, plotting)
     }
   }
   
-  plotm1 <- plotm1[!duplicated(plotm1),]
-  plotm1 <- plotm1[-1,]
-  plotm1 <- reshape2::melt(plotm1, id.var = c("Method", "SampleSize"))
-  #plotm1 <- plotm1[!duplicated(plotm1),]
-  
- # plotm1 <- plotting
-  #plotm1 <- plotting[plotting$Method == meth,]
- # plotm1 <- plotm1[!duplicated(plotm1[,1:3]),] ####NOTE TO SELF: do we use best_tx which always chooses a best, or plot t1 errors only (ie, chose best when no best)
-  #+ facet_grid(. ~ Method) removed 
  
-  fm <- ggplot(plotm1, aes(x = SampleSize, y = value, fill = variable)) +
+
+  plotm1 <- plotm1[!is.na(plotm1$`Treatment 1`),]
+  
+  plotm1[,1:4] <- plotm1[,1:4]/rowSums(plotm1[,1:4])
+  
+  plotm1 <- reshape2::melt(plotm1, id.var = c("Method", "Sample Size"))
+  
+  fm <- ggplot(plotm1, aes(x =`Sample Size`, y = value, fill = variable)) +
     geom_col() + scale_x_continuous(breaks = n)+ facet_grid(. ~ Method)+ylab("Proportion of Bests") + xlab("Sample Size")+labs(fill = NULL)+theme_minimal()+
     theme(
       plot.title.position = "plot",
@@ -106,4 +84,5 @@ plot_nullbest <- function(outputs, .font_size = font_size, .pt_size = pt_size, N
            shape = guide_legend(ncol = 2)) #+ ggtitle()
   
   return(fm)
+  
 }
