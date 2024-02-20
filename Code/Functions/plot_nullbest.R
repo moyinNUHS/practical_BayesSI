23356a7949784823e9aa7e4cddf5b67d2c99a90c
rebeccaperezMGH <131869387+rebeccaperezMGH@users.noreply.github.com>
2024-02-20 16:04:50 +0800

    Update plot_nullbest.R

--- Code/Functions/plot_nullbest.R
+++ Code/Functions/plot_nullbest.R
@@ -1,16 +1,15 @@
-
 plot_nullbest <- function(outputs, .font_size = font_size, .pt_size = pt_size, N_iter, ind) {
   
-  null_data <- as.data.frame(matrix(NA, nrow = 1, ncol = 6)) ##create empty dataframes to store data
-  alliter_data <- as.data.frame(matrix(NA, nrow = 1, ncol = 6))
-  colnames(null_data) <- c("pattern 1", "pattern 2", "pattern 3", "pattern 4", "Method", "SampleSize")
-  colnames(alliter_data) <- c("pattern 1", "pattern 2", "pattern 3", "pattern 4", "Method", "SampleSize")
- 
+  null_data <- as.data.frame(matrix(NA, nrow = 1, ncol = 3)) ##create empty dataframes to store data
+  alliter_data <- as.data.frame(matrix(NA, nrow = 1, ncol = 3))
+  colnames(null_data) <- c("pattern 4", "Method", "SampleSize")
+  colnames(alliter_data) <- c("pattern 4", "Method", "SampleSize")
+  
   ###sample sizes vector 
-   n = parse_number(names(outputs[[ind]]))
+  n = parse_number(names(outputs[[ind]]))
   ##subset to scenario 
   d <- outputs[[ind]]
-
+  
   for (i in n) {
     
     sub_ind <- grep(paste("=",as.character(i), sep=" "), names(d)) 
@@ -20,10 +19,14 @@ plot_nullbest <- function(outputs, .font_size = font_size, .pt_size = pt_size, N
       
       subset_iter = as.data.frame(subset_n[[a]]$identified_best_t) ##subset by iter
       
+      subset_iter <- as.data.frame(subset_iter$'pattern 4') ##subset to only pattern which compares all treatments (pattern 4)
+      
+      colnames(subset_iter) <- "pattern 4"
+      
       subset_iter$Method <- rownames(subset_iter) ##record methods in column 
       
       subset_iter$SampleSize <- i ##record sample size in column 
-
+      
       
       alliter_data <- rbind(alliter_data, subset_iter) ##combine iterations into one df 
       
@@ -36,7 +39,7 @@ plot_nullbest <- function(outputs, .font_size = font_size, .pt_size = pt_size, N
   }
   
   
-  null_data <- null_data[!is.na(null_data$`pattern 1`),] ##remove NAs 
+  null_data <- null_data[!is.na(null_data$`pattern 4`),] ##remove NAs 
   
   plotting <- as.data.frame(matrix(NA, nrow = 1, ncol = 6))
   colnames(plotting) <- c("Treatment 1", "Treatment 2", "Treatment 3", "Treatment 4", "Method", "Sample Size")
@@ -46,28 +49,27 @@ plot_nullbest <- function(outputs, .font_size = font_size, .pt_size = pt_size, N
     for (m in unique(null_data$Method)){
       sample_meth <- subset(null_data, SampleSize == q & Method == m)
       ##subset for each method and sample size 
-     plotting[1,1] <- sum(sample_meth[, c(2,4)] == 1) / (nrow(sample_meth) *4) #* 2) 
-     plotting[1,2] <- sum(sample_meth[, 1:4] == 2) / (nrow(sample_meth) * 4) ###sum number of occurrences of T2 over # of patterns that T2 is present in (=2)
-     plotting[1,3] <- sum(sample_meth[, 1:4] == 3) / (nrow(sample_meth) * 4)
-     plotting[1,4] <- sum(sample_meth[, 3:4] == 4) / (nrow(sample_meth) *4) #* 2) ##Currently does not take into account # of avail treatments to keep probability sum = 1 
-     plotting[1,5] <- m ##record method 
-     plotting[1,6] <- q ##record sample size 
-     
+      plotting[1,1] <- sum(sample_meth[, 1] == 1) / (nrow(sample_meth)) ## sum of times Tx 1 = identified_best_t / # iterations per sample size and method 
+      plotting[1,2] <- sum(sample_meth[, 1] == 2) / (nrow(sample_meth)) 
+      plotting[1,3] <- sum(sample_meth[, 1] == 3) / (nrow(sample_meth))
+      plotting[1,4] <- sum(sample_meth[, 1] == 4) / (nrow(sample_meth)) 
+      plotting[1,5] <- m ##record method 
+      plotting[1,6] <- q ##record sample size 
+      
       
       plotm1 <- rbind(plotm1, plotting)
     }
   }
   
- 
-
-  plotm1 <- plotm1[!is.na(plotm1$`Treatment 1`),] ##remove NAs 
   
-  #plotm1[,1:4] <- plotm1[,1:4]/rowSums(plotm1[,1:4])
+  
+  plotm1 <- plotm1[!is.na(plotm1$`Treatment 4`),] ##remove NAs 
+  
   
   plotm1 <- reshape2::melt(plotm1, id.var = c("Method", "Sample Size"))
   
   fm <- ggplot(plotm1, aes(x =`Sample Size`, y = value, fill = variable)) +
-    geom_col() + scale_x_continuous(breaks = c(1000, 2000, 3000, 4000, 5000))+ facet_grid(. ~ Method)+ylab("Proportion of Bests") + xlab("Sample Size")+labs(fill = NULL)+theme_minimal()+ scale_y_continuous(breaks = c(0, 0.25, 0.5, 0.75, 1.0), limits = c(0, 1.0)) +
+    geom_bar(position = "dodge", stat = "identity") + scale_x_continuous(breaks = c(1000, 2000, 3000, 4000, 5000)) + scale_y_continuous(breaks = c(0, 0.25, 0.5), limits = c(0, 0.5)) + facet_grid(. ~ Method)+ylab("Proportion of Bests") + xlab("Sample Size")+labs(fill = NULL)+theme_minimal() +
     theme(
       plot.title.position = "plot",
       legend.position = "bottom",
