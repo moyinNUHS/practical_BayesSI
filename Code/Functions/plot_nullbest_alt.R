f7bf5ddf3d9b538e0fa7ca5ed6faca651d3b54e9
rebeccaperezMGH <131869387+rebeccaperezMGH@users.noreply.github.com>
2024-04-24 17:11:39 +0800

    Update plot_nullbest_alt.R

--- Code/Functions/plot_nullbest_alt.R
+++ Code/Functions/plot_nullbest_alt.R
@@ -66,7 +66,7 @@ plot_nullbest_alt <- function(outputs, .font_size = font_size, .pt_size = pt_siz
   plotm1 <- reshape2::melt(plotm1, id.var = c("Method", "Sample Size"))
   
   fm <- ggplot(plotm1, aes(x =`Sample Size`, y = value, fill = variable)) +
-    geom_bar(position = "dodge", stat = "identity") + scale_x_continuous(breaks = c(1000, 2000, 3000, 4000, 5000))+ scale_y_continuous(breaks = c(0, 0.25, 0.5), limits = c(0, 0.5)) + facet_grid(. ~ Method)+ylab("Proportion of Bests") + xlab("Sample Size")+labs(fill = NULL)+theme_minimal()+
+    geom_bar(position = "dodge", stat = "identity") + scale_x_continuous(breaks = c(1000, 2000, 3000, 4000, 5000))+ scale_y_continuous(breaks = c(0, 0.1, 0.2, 0.3, 0.4, 0.5), limits = c(0, 0.5)) + facet_grid(. ~ Method)+ylab("Proportion of Bests") + xlab("Sample Size")+labs(fill = NULL)+theme_minimal()+
     geom_hline(yintercept = 0.25, linetype = "longdash", colour = "grey")+
     theme(
       plot.title.position = "plot",
